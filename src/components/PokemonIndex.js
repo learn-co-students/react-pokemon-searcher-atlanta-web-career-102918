3acc86dc53930f0d59cd998c7e2c7f3dd16e403f
Kaeland Chatman <kaeland1@gmail.com>
2019-01-22 15:41:46 -0500

    committed

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,51 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: null,
+    loading: true, 
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(res => res.json())
+      .then(pokemon => this.setState({ pokemon, loading: false }));
+  }
+
+  handleSearchChange = (e, { value }) => {
+    let filteredPokemon = this.state.pokemon.filter((pokemon) => {
+      return pokemon === value
+    }) 
+    this.setState({ pokemon: filteredPokemon })
+  }
+
   render() {
+    const { pokemon, loading } = this.state;
     return (
       <div>
-        <h1>Pokemon Searcher</h1>
-        <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
-        <br />
-        <PokemonCollection />
-        <br />
-        <PokemonForm />
+        {loading === true ? (
+          <p>Loading</p>
+        ) : (
+          <div>
+            <h1>Pokemon Searcher</h1>
+            <br />
+            <Search
+              onSearchChange={_.debounce(() => this.handleSearchChange, 500)}
+              showNoResults={false}
+            />
+            <br />
+            <PokemonCollection pokemon={pokemon} />
+            <br />
+            <PokemonForm />
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
