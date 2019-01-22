61d973792c44df4526fc9c1524669c6a006e0e2e
github username <github email address>
2019-01-22 14:51:17 -0500

    add pokemon data && get img to flip

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    card: true
+  }
+
+  // findHP = () => {
+  //   return this.props.pokemon.stats.find(stat => stat.name === 'hp').value
+  // }
+
+  flipCard = () => {
+    this.setState({card: !this.state.card})
+  }
+
   render() {
+
+    let pokemon = this.props.pokemon
+    let imgFlip = this.state.card ? pokemon.sprites.front : pokemon.sprites.back
+
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div onClick={() => this.flipCard()} className="image">
+            <img src={imgFlip} alt={pokemon.id} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.stats[5].value} hp
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -4,15 +4,30 @@ import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
+const API = `http://localhost:3000/pokemon`
+
 class PokemonPage extends React.Component {
+  state = {
+    pokemonArray: []
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(pokemon => this.setState({pokemonArray: pokemon}))
+  }
+
+
   render() {
+
+    console.log(this.state.pokemonArray)
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
         <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonArray={this.state.pokemonArray} />
         <br />
         <PokemonForm />
       </div>
